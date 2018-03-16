9f4bc8cd5f222b9cd066ffca253f64068feabad2
ldl729066951 <729066951@qq.com>
2018-03-16 23:47:38 +0800

    add  Junit 4 1. 可以避免测试点的遗漏，为了更好的进行测试，可以提高测试效率 2. 可以自动测试，可以在项目打包前进行测试校验 3. 可以及时发现因为修改代码导致新的问题的出现，并及时解决

---
+++ src/main/java/com/castor/web/service/HelloService.java
@@ -0,0 +1,12 @@
+package com.castor.web.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String getName() {
+        return "hello";
+    }
+
+}
